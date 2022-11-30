e66a3f34c9ac2794984f1ca834a8c0988a0be814
Nikko Bansil <bjohnnikko@gmail.com>
2022-11-29 19:49:08 -0800

    added the cookie username variable to templateVars

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require("cookie-parser");
 
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -33,12 +35,14 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  console.log(req.cookies['username']);
+  const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies['username'] }
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -66,7 +70,7 @@ app.post("/login", (req, res) => {
 
 app.get("/urls/:id", (req, res) => {
   const long = urlDatabase[req.params.id];
-  const templateVars = { id: req.params.id, longURL: long };
+  const templateVars = { id: req.params.id, longURL: long, username: req.cookies['username'] };
   res.render("urls_show", templateVars);
 });
 
