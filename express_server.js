e87fa4ccb9018362911bf3dd26ef2654989e6487
Nikko Bansil <bjohnnikko@gmail.com>
2022-12-01 14:16:40 -0800

    changed cookie creation to use encrypted cookies and also to check these encrypted cookies

--- express_server.js
+++ express_server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 const bcrypt = require('bcryptjs');
 
 app.set("view engine", "ejs");
-app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['vSSalErXAFaJ48lWaXa8We']
+}));
 
 const urlDatabase = {
   "b2xVn2": {
@@ -62,31 +65,31 @@ const urlsForUser = (id) => {
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  if(!req.cookies['user_id']) {
+  if(!req.session.user_id) {
     res.redirect(`/urls`);
   }
   res.redirect(`/login`);
 });
 
 app.get("/urls", (req, res) => {
-  if (req.cookies['user_id'] === undefined) {
+  if (req.session.user_id === undefined) {
     res.status(200).send("Please login to view the URLS");
   }
-  const templateVars = { urls: urlsForUser(req.cookies['user_id']), user: users[req.cookies['user_id']] };
+  const templateVars = { urls: urlsForUser(req.session.user_id), user: users[req.session.user_id] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  if (req.cookies['user_id'] === undefined) {
+  if (req.session.user_id === undefined) {
     res.redirect(`/login`);
   }
-  const templateVars = { user: users[req.cookies['user_id']] };
+  const templateVars = { user: users[req.session.user_id] };
   res.render("urls_new", templateVars);
 });
 
 app.get("/register", (req, res) => {
-  if (req.cookies['user_id'] === undefined) {
-    const templateVars = { user: users[req.cookies['user_id']] };
+  if (req.session.user_id === undefined) {
+    const templateVars = { user: users[req.session.user_id] };
     res.render("urls_register", templateVars);
   }
   res.redirect(`/urls/`);
@@ -106,26 +109,26 @@ app.post("/register", (req, res) => {
   }
 
   users[id] = { id: id, email: email, password: hashedPassword };
-  res.cookie('user_id', id);
+  req.session.user_id = id;
   res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
-  if (req.cookies['user_id'] === undefined) {
+  if (req.session.user_id === undefined) {
     res.status(200).send("Only logged in users are allowed to shorten URLs. Please log in to use this feature.");
   }
   const id = generateRandomString();
-  urlDatabase[id] = { longURL: req.body.longURL, userID: req.cookies['user_id'] };
+  urlDatabase[id] = { longURL: req.body.longURL, userID: req.session.user_id };
   res.redirect(`/urls/${id}`);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   if (req.params.id in urlDatabase) {
-    if (req.cookies['user_id'] === undefined) {
+    if (req.session.user_id === undefined) {
       res.status(200).send("Please log in to view this URL");
     }
 
-    if (req.cookies['user_id'] !== urlDatabase[req.params.id].userID) {
+    if (req.session.user_id !== urlDatabase[req.params.id].userID) {
       res.status(200).send("You do not have access to this URL");
     }
     const id = req.params.id;
@@ -137,11 +140,11 @@ app.post("/urls/:id/delete", (req, res) => {
 
 app.post("/urls/:id", (req, res) => {
   if (req.params.id in urlDatabase) {
-    if (req.cookies['user_id'] === undefined) {
+    if (req.session.user_id === undefined) {
       res.status(200).send("Please log in to view this URL");
     }
 
-    if (req.cookies['user_id'] !== urlDatabase[req.params.id].userID) {
+    if (req.session.user_id !== urlDatabase[req.params.id].userID) {
       res.status(200).send("You do not have access to this URL");
     }
     const id = req.params.id;
@@ -152,8 +155,8 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  if (req.cookies['user_id'] === undefined) {
-    const templateVars = { user: users[req.cookies['user_id']] };
+  if (req.session.user_id === undefined) {
+    const templateVars = { user: users[req.session.user_id] };
     res.render(`urls_login`, templateVars);
   }
   res.redirect(`/urls/`);
@@ -171,26 +174,26 @@ app.post("/login", (req, res) => {
     res.status(403).send("Incorrect password");
   }
   
-  res.cookie('user_id', getUserByEmail(email).id);
+  req.session.user_id = getUserByEmail(email).id;
   res.redirect(`/urls`);
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
+  req.session = null;
   res.redirect(`/login`);
 });
 
 app.get("/urls/:id", (req, res) => {
   if (req.params.id in urlDatabase) {
-    if (req.cookies['user_id'] === undefined) {
+    if (req.session.user_id === undefined) {
       res.status(200).send("Please log in to view this URL");
     }
 
-    if (req.cookies['user_id'] !== urlDatabase[req.params.id].userID) {
+    if (req.session.user_id !== urlDatabase[req.params.id].userID) {
       res.status(200).send("You do not have access to this URL");
     }
     const long = urlDatabase[req.params.id].longURL;
-    const templateVars = { id: req.params.id, longURL: long, user: users[req.cookies['user_id']]};
+    const templateVars = { id: req.params.id, longURL: long, user: users[req.session.user_id]};
     res.render("urls_show", templateVars);
   }
   res.status(200).send("ID do not exist. Please enter a correct ID");
