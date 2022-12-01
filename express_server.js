7227bceec7b9cec2edac5edd06ebd53832956c1a
Nikko Bansil <bjohnnikko@gmail.com>
2022-11-30 18:20:12 -0800

    changed database structure

--- express_server.js
+++ express_server.js
@@ -7,18 +7,24 @@ app.set("view engine", "ejs");
 app.use(cookieParser());
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "aJ48lW"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "aXa8We"
+  }
 };
 
 const users = {
   userRandomID: {
-    id: "userRandomID",
+    id: "aJ48lW",
     email: "user@example.com",
     password: "purple-monkey-dinosaur",
   },
   user2RandomID: {
-    id: "user2RandomID",
+    id: "aXa8We",
     email: "user2@example.com",
     password: "dishwasher-funk",
   },
